83c54a0e1ad13b15ad6d05f48a46b39ae6661724
Amreet711 <sunny1935@gmail.com>
2026-06-20 13:22:40 +1000

    Result Calculator (Update 2)

--- Result Calculator.py
+++ Result Calculator.py
@@ -11,13 +11,13 @@ age = int(input("How old are you? "))
 grade = int(input("What grade are you in? "))
 
 # Student's English result
-result1 = int(input("What was your result for English? "))
+result1 = float(input("What was your result for English? "))
 
 # Student's Maths result
-result2 = int(input("What was your result for Maths? "))
+result2 = float(input("What was your result for Maths? "))
 
 # Student's Science result
-result3 = int(input("What was your result for Science? "))
+result3 = float(input("What was your result for Science? "))
 
 # Student's total score
 total = result1 + result2 + result3
@@ -35,6 +35,19 @@ print("Student's Maths score:", result2, "/100")
 print("Student's Science score:", result3, "/100")
 print("Student's total score:", total, "/300")
 print("Student's average score:", average, "/100")
+
+if average >= 90:
+    print("Student's examination grade: A+")
+elif average >= 80:
+    print("Student's examination grade: A")
+elif average >= 70:
+    print("Student's examination grade: B")
+elif average >= 60:
+    print("Student's examination grade: C")
+elif average >= 50:
+    print("Student's examination grade: D")
+else:
+    print("Student's examination grade: F")
 
 if average >= 50:
     print("Student's result: PASS")
